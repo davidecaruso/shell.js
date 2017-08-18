b75098d8454290c6363c763c24c4dab7dc792939
Davide Caruso <davide.caruso93@gmail.com>
2017-08-18 18:02:12 +0200

    Add new dependencies and also add new Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,42 @@
-var gulp = require('gulp'),
-  jshint = require('gulp-jshint'),
-  compass = require('gulp-compass'),
-  sourcemaps = require('gulp-sourcemaps'),
-  util = require('gulp-util'),
-  dist = true === util.env.dist;
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var rename = require('gulp-rename');
+var compass = require('gulp-compass');
+var pump = require('pump');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var util = require('gulp-util');
+var dist = true === util.env.dist;
+var paths = {
+  source: 'src/',
+  destination: 'dist/'
+};
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['compass', 'jshint', 'watch']);
 
-gulp.task('jshint', function() {
-  return gulp.src('src/js/**/*.js')
+gulp.task('jshint', function () {
+  return gulp.src(paths.source + 'js/**/*.js')
     .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'))
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
+gulp.task('scripts', function() {
+  return gulp.src(paths.source + 'js/**/*.js')
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.destination + 'js'));
+});
+
 gulp.task('compass', function () {
-  gulp.src('./src/*.scss')
-    .pipe(sourcemaps.init())
+  gulp.src(paths.source + 'sass/**/*.scss')
+    .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(compass({
-      css:      'dist/css',
-      sass:     'src/sass',
+      css:      paths.destination + 'css',
+      sass:     paths.source + 'sass',
       style:    dist ? "compressed" : "expanded",
       comments: true,
       force:    true
@@ -27,7 +45,7 @@ gulp.task('compass', function () {
     .pipe(gulp.dest('tmp/css'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('src/js/**/*.js', ['jshint']);
-  gulp.watch('src/sass/**/*.scss', ['compass']);
+gulp.task('watch', function () {
+  gulp.watch(paths.source + 'js/**/*.js', ['jshint', 'scripts']);
+  gulp.watch(paths.source + 'sass/**/*.scss', ['compass']);
 });
