846651c1391d8e572e63fb2f2f5e9cecf0b2a113
Davide Caruso <davide.caruso@blastingnews.com>
2017-09-01 18:39:10 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,59 @@
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
 var rename = require('gulp-rename');
 var compass = require('gulp-compass');
+var eslint = require('gulp-eslint');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
+var webpack = require("webpack-stream");
 var util = require('gulp-util');
 var dist = true === util.env.dist;
 var paths = {
-  source: 'src/',
+  source:      'src/',
   destination: 'dist/'
 };
 
-gulp.task('default', ['styles', 'scripts', 'watch']);
+/// Lint task
+gulp.task('lint', () => {
+  return gulp.src('src/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
 
-gulp.task('scripts', function () {
-  return gulp.src(paths.source + 'js/**/*.js')
-     /// JSHint
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(jshint.reporter('fail'))
-    .pipe(jshint.reporter('jshint-stylish'))
-    /// Uglify
-    .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(uglify())
-    .pipe(rename({ suffix: '.min' }))
+/// Build task
+gulp.task('build', () => {
+  return gulp.src(paths.source + 'js/shell.js')
+    .pipe(webpack(require("./webpack.config.js")))
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify({
+      preserveComments: "license",
+      compress:         {
+        negate_iife: false
+      }
+    }))
+    .pipe(rename('shell.min.js'))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(paths.destination + 'js'));
 });
 
-gulp.task('styles', function () {
+/// Compass task
+gulp.task('styles', () => {
   gulp.src(paths.source + 'sass/**/*.scss')
-     /// Compass
     .pipe(compass({
-      css:      paths.destination + 'css',
-      sass:     paths.source + 'sass',
-      style:    dist ? "compressed" : "expanded",
-      comments: true,
+      css:       paths.destination + 'css',
+      sass:      paths.source + 'sass',
+      style:     dist ? "compressed" : "expanded",
+      comments:  true,
       sourcemap: true,
-      force:    true
+      force:     true
     }))
 });
 
-gulp.task('watch', function () {
-  gulp.watch(paths.source + 'js/**/*.js', ['scripts']);
+/// Watch task
+gulp.task('watch', () => {
   gulp.watch(paths.source + 'sass/**/*.scss', ['styles']);
+  gulp.watch(paths.source + 'js/**/*.js', ['lint', 'build']);
 });
+
+/// Default task
+gulp.task('default', ['styles', 'lint', 'build', 'watch']);
