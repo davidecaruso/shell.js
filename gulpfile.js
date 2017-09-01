5660ed74dbf19f55fbe231dd6d0be5b91e55a0d2
Davide Caruso <davide.caruso@blastingnews.com>
2017-09-01 13:00:25 +0200

    Update Gulp configuration

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var rename = require('gulp-rename');
 var compass = require('gulp-compass');
-var pump = require('pump');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var util = require('gulp-util');
@@ -12,40 +11,37 @@ var paths = {
   destination: 'dist/'
 };
 
-gulp.task('default', ['compass', 'jshint', 'watch']);
+gulp.task('default', ['styles', 'scripts', 'watch']);
 
-gulp.task('jshint', function () {
+gulp.task('scripts', function () {
   return gulp.src(paths.source + 'js/**/*.js')
+     /// JSHint
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
     .pipe(jshint.reporter('fail'))
-    .pipe(jshint.reporter('jshint-stylish'));
-});
-
-gulp.task('scripts', function() {
-  return gulp.src(paths.source + 'js/**/*.js')
+    .pipe(jshint.reporter('jshint-stylish'))
+    /// Uglify
     .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
-    .pipe(sourcemaps.write())
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(paths.destination + 'js'));
 });
 
-gulp.task('compass', function () {
+gulp.task('styles', function () {
   gulp.src(paths.source + 'sass/**/*.scss')
-    .pipe(sourcemaps.init({ loadMaps: true }))
+     /// Compass
     .pipe(compass({
       css:      paths.destination + 'css',
       sass:     paths.source + 'sass',
       style:    dist ? "compressed" : "expanded",
       comments: true,
+      sourcemap: true,
       force:    true
     }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('tmp/css'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch(paths.source + 'js/**/*.js', ['jshint', 'scripts']);
-  gulp.watch(paths.source + 'sass/**/*.scss', ['compass']);
+  gulp.watch(paths.source + 'js/**/*.js', ['scripts']);
+  gulp.watch(paths.source + 'sass/**/*.scss', ['styles']);
 });
