a350dfd265d5069e412c0aed861df20a2b523de4
Davide Caruso <davide.caruso93@gmail.com>
2018-06-07 13:26:31 +0200

    Improve util tests adding contexts

--- test/util.test.ts
+++ test/util.test.ts
@@ -15,24 +15,33 @@ describe('Util', () => {
         it('should exist', () => {
             expect(Util.$).exist;
         });
-        it('should return value which has "length" property', () => {
-            expect("length" in Util.$('random-string', document)).to.true;
+        context('when selector doesn\'t exist inside the DOM', () => {
+            it('should return value which has "length" property', () => {
+                expect("length" in Util.$('random-string', document)).to.true;
+            });
+            it('should return value which has 0 as value of "length" property', () => {
+                expect(Util.$('random-string', document).length).to.be.equal(0);
+            });
         });
-        it('should return value which has 0 as value of "length" property', () => {
-            expect(Util.$('random-string', document).length).to.be.equal(0);
+        context('when selector exists inside the DOM', () => {
+            it('should return value which has 1 as value of "length" property', () => {
+                expect(Util.$('.foo', document).length).to.be.equal(1);
+            });
+            it('should return value which has one element', () => {
+                expect(Util.$('.foo', document)[0]).exist;
+            });
+            it('should return value which has the expected HTML element', () => {
+                expect(Util.$('.foo', document)[0].innerHTML).to.be.equal('<p class="bar">Foobar</p>');
+            });
         });
-        it('should return value which has 1 as value of "length" property', () => {
-            expect(Util.$('.foo', document).length).to.be.equal(1);
+        context('when selector exists inside another element', () => {
+            it('should return value which has the expected HTML element passing also the "parent" selector', () => {
+                let parent = Util.$('.foo', document)[0];
+                expect(Util.$('.bar', parent)[0].innerHTML).to.be.equal('Foobar');
+            });
         });
-        it('should return value which has one element', () => {
-            expect(Util.$('.foo', document)[0]).exist;
-        });
-        it('should return value which has the expected HTML element', () => {
-            expect(Util.$('.foo', document)[0].innerHTML).to.be.equal('<p class="bar">Foobar</p>');
-        });
-        it('should return value which has the expected HTML element passing also the "parent" selector', () => {
-            let parent = Util.$('.foo', document)[0];
-            expect(Util.$('.bar', parent)[0].innerHTML).to.be.equal('Foobar');
+        after(() => {
+           document = null;
         });
     });
 });
