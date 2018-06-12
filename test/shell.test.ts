2d2c2113addbd08d298e1802a5054c9e81b03413
Davide Caruso <davide.caruso93@gmail.com>
2018-06-12 15:18:40 +0200

    ✅ Add tests for main.ts

--- test/shell.test.ts
+++ test/shell.test.ts
@@ -1,10 +1,11 @@
 import {should} from "should";
 import {expect} from "chai";
-import Typed from "typed.js";
+
 const JSDOM = (require("jsdom")).JSDOM;
-const GLOBAL:any = global;
+const GLOBAL: any = global;
 GLOBAL.window = new JSDOM(`<!doctype html><html><body></body></html>`).window;
 GLOBAL.document = window.document;
+
 let Shell = require("../src/js/main");
 
 describe("Shell", () => {
@@ -79,7 +80,7 @@ describe("Shell", () => {
             );
         });
 
-        context("when no options have been passed", () => {
+        context("when not have been passed", () => {
             let shell;
             before(() => {
                 shell = new Shell();
@@ -96,7 +97,7 @@ describe("Shell", () => {
             });
         });
 
-        context("when options have been passed", () => {
+        context("when have been passed", () => {
             let div, shell;
 
             before(() => {
@@ -106,48 +107,43 @@ describe("Shell", () => {
             });
 
             describe("\"options.style\" property", () => {
-                context("case \"default\"", () => {
-                    before(() => {
-                        shell = new Shell('#shell', {style: 'default'});
-                    });
+                describe("HTML element", () => {
+                    context("when style is \"default\"", () => {
+                        before(() => {
+                            shell = new Shell('#shell', {style: 'default'});
+                        });
 
-                    describe("HTML element", () => {
-                        it("should have class", () => {
+                        it("should not have \"default\" class", () => {
                             expect(div.classList.contains("default")).to.be.false;
                         });
                     });
-                });
-                context("case \"osx\"", () => {
-                    before(() => {
-                        shell = new Shell('#shell', {style: 'osx'});
-                    });
 
-                    describe("HTML element", () => {
-                        it("should have class", () => {
+                    context("when style is \"osx\"", () => {
+                        before(() => {
+                            shell = new Shell('#shell', {style: 'osx'});
+                        });
+
+                        it("should have \"osx\" class", () => {
                             expect(div.classList.contains("osx")).to.be.true;
                         });
                     });
-                });
 
-                context("case \"ubuntu\"", () => {
-                    before(() => {
-                        shell = new Shell('#shell', {style: 'ubuntu'});
-                    });
+                    context("when style is \"ubuntu\"", () => {
+                        before(() => {
+                            shell = new Shell('#shell', {style: 'ubuntu'});
+                        });
 
-                    describe("HTML element", () => {
-                        it("should have class", () => {
+                        it("should have \"ubuntu\" class", () => {
                             expect(div.classList.contains("ubuntu")).to.be.true;
                         });
                     });
-                });
 
-                context("case \"windows\"", () => {
-                    before(() => {
-                        shell = new Shell('#shell', {style: 'windows'});
-                    });
+                    context("when style is \"windows\"", () => {
+                        before(() => {
+                            shell = new Shell('#shell', {style: 'windows'});
+                        });
 
-                    describe("HTML element", () => {
-                        it("should have class", () => {
+                        it("should have \"windows\" class", () => {
                             expect(div.classList.contains("windows")).to.be.true;
                         });
                     });
@@ -155,77 +151,28 @@ describe("Shell", () => {
             });
 
             describe("\"options.commands\" property", () => {
-        //         context('when present', () => {
-        //             it('should have commands', () => {
-        //                 let shell = new Shell('#shell', {commands: ['foo', 'bar']});
-        //                 expect(shell.options.commands.length).to.equal(2);
-        //             });
-        //             context('when one command contains "sudo"', () => {
-        //                 context('if style is "ubuntu" or "default"', () => {
-        //                     it('should print an additional line', () => {
-        //                         let shell = new Shell('#shell', {style: 'ubuntu', commands: ['sudo']});
-        //                         expect(shell.el[0].innerHTML).to.match(/class="command output">\[sudo\] password/);
-        //                     });
-        //                 });
-        //                 context('if style is "osx"', () => {
-        //                     it('should print an additional line', () => {
-        //                         let shell = new Shell('#shell', {style: 'osx', commands: ['sudo']});
-        //                         expect(shell.el[0].innerHTML).to.match(/class="command output">Password:/);
-        //                     });
-        //                 });
-        //                 context('if style is "windows"', () => {
-        //                     it('should print an additional line', () => {
-        //                         let shell = new Shell('#shell', {style: 'windows', commands: ['sudo']});
-        //                         expect(shell.el[0].innerHTML).to.match(/class="command output">bash: sudo: command not found/);
-        //                     });
-        //                 });
-        //             });
-        //             context('when one command contains "exit"', () => {
-        //                 context('if style is "windows"', () => {
-        //                     it('should print an additional line', () => {
-        //                         let shell = new Shell('#shell', {style: 'windows', commands: ['exit']});
-        //                         expect(shell.el[0].innerHTML).to.match(/class="command output">bash: exit: command not found</);
-        //                     });
-        //                 });
-        //                 context('else', () => {
-        //                     it('should print an additional line', () => {
-        //                         let shell = new Shell('#shell', {commands: ['exit']});
-        //                         expect(shell.el[0].innerHTML).to.match(/class="command output">logout</);
-        //                     });
-        //                 });
-        //             });
-        //         });
-        //     });
-        //
-        //     describe('Responsive', () => {
-        //         context('when true', () => {
-        //             it('element should have "responsive" class', () => {
-        //                 let shell = new Shell('#shell', {responsive: true});
-        //                 expect(shell.el[0].className).to.contains('responsive');
-        //             });
-        //         });
-        //     });
-        //
-        //     describe('Root', () => {
-        //         context('when true', () => {
-        //             it('user prefix should be root', () => {
-        //                 let shell = new Shell('#shell', {root: true});
-        //                 expect(shell.buildPrefix()).to.match(/class="user">root@<\//);
-        //             });
-        //         });
-        //     });
-        //
-        //     describe('Typed', () => {
-        //         context('when is not false', () => {
-        //             it('should be a function', () => {
-        //                 let shell = new Shell('#shell', {typed: Typed});
-        //             });
-        //             context('when there are commands', () => {
-        //                 it('should be invoked', () => {
-        //                     let shell = new Shell('#shell', {typed: Typed, commands: ['foo', 'bar', 'sudo foobar']});
-        //                 });
-        //             });
-        //         });
+                context("when have been passed", () => {
+                    it("should be the as same number", () => {
+                        shell = new Shell("#shell", {commands: ["foo", "bar"]});
+                        expect(shell.options.commands.length).to.equal(2);
+                    });
+                    it("should be the same", () => {
+                        shell = new Shell("#shell", {commands: ["foo", "bar"]});
+                        expect(shell.options.commands[0]).to.equal("foo");
+                        expect(shell.options.commands[1]).to.equal("bar");
+                    });
+                });
+            });
+
+            describe("\"options.responsive\" property", () => {
+                context("when is true", () => {
+                    describe("HTML element", () => {
+                        it("should have \"responsive\" class", () => {
+                            shell = new Shell('#shell', {responsive: true});
+                            expect(div.classList.contains("responsive")).to.be.true;
+                        });
+                    });
+                });
             });
 
             after(() => {
@@ -233,35 +180,4 @@ describe("Shell", () => {
             });
         });
     });
-
-    // describe('Methods', () => {
-    //     describe('buildStatusBar()', () => {
-    //         context('when invoked', () => {
-    //             it('should return HTML string', () => {
-    //                 expect(typeof (new Shell()).buildStatusBar()).to.equal('string');
-    //             });
-    //         });
-    //     });
-    //     describe('buildContent()', () => {
-    //         context('when invoked', () => {
-    //             it('should return HTML string', () => {
-    //                 expect(typeof (new Shell()).buildContent()).to.equal('string');
-    //             });
-    //         });
-    //     });
-    //     describe('buildLine()', () => {
-    //         context('when invoked', () => {
-    //             it('should return HTML string', () => {
-    //                 expect(typeof (new Shell()).buildLine()).to.equal('string');
-    //             });
-    //         });
-    //     });
-    //     describe('buildPrefix()', () => {
-    //         context('when invoked', () => {
-    //             it('should return HTML string', () => {
-    //                 expect(typeof (new Shell()).buildPrefix()).to.equal('string');
-    //             });
-    //         });
-    //     });
-    // });
 });
